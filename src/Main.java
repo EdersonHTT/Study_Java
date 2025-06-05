6453f2969e5c46f3ef2431b01bd8b577ef2354ce
EdersonSS <166855651+EdersonHTT@users.noreply.github.com>
2025-06-05 17:29:12 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class PrimeiraClasse {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("Concluí a aula 01 e agora estou mergulhando em Java!");
+
+    }
+
+}
